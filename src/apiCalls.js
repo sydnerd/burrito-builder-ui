067f00e899789f6d0b290575bf55bbca602e2f13
Sydney Bear <sbear3915@gmail.com>
2021-08-10 09:47:05 -0600

    Create addOrder post method in API calls file

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,11 @@ export const getOrders = () => {
   return fetch('http://localhost:3001/api/v1/orders')
       .then(response => response.json())
 }
+
+export const addOrder = (newBurrito) => {
+  return fetch('http://localhost:3001/api/v1/orders', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(newBurrito)
+  })
+}
