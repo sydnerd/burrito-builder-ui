ba599e2c32cc1af3b7a24db4fd69f9e03fb9be35
Sydney Bear <sbear3915@gmail.com>
2021-08-10 09:41:28 -0600

    Add a function that updates the state of the ingredients

--- src/components/OrderForm/OrderForm.js
+++ src/components/OrderForm/OrderForm.js
@@ -10,10 +10,15 @@ class OrderForm extends Component {
     };
   }
 
+  handleIngredientChange = (e, newIngredient) => {
+    e.preventDefault();
+    this.setState({ingredients: [...this.state.ingredients, newIngredient]})
+  }
+
   handleNameChange = e => {
     this.setState({[e.target.name]: e.target.value})
   }
-  
+
   handleSubmit = e => {
     e.preventDefault();
     this.clearInputs();
@@ -27,7 +32,7 @@ class OrderForm extends Component {
     const possibleIngredients = ['beans', 'steak', 'carnitas', 'sofritas', 'lettuce', 'queso fresco', 'pico de gallo', 'hot sauce', 'guacamole', 'jalapenos', 'cilantro', 'sour cream'];
     const ingredientButtons = possibleIngredients.map(ingredient => {
       return (
-        <button key={ingredient} name={ingredient} onClick={e => this.handleIngredientChange(e)}>
+        <button key={ingredient} name={ingredient} onClick={e => this.handleIngredientChange(e, ingredient)}>
           {ingredient}
         </button>
       )
